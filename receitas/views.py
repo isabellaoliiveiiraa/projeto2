a5b1659eead86ac77a23d09e0a0c37244b65c1f3
isabellaoliiveiiraa <bellacsoliveira123@gmail.com>
2025-08-19 20:54:37 -0300

    final da aula

--- receitas/views.py
+++ receitas/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404
+
 
 # Create your views here.
 def home(request):
     return render(request, 'receitas/home.html')
+
+def receita(request, receita_id):
+
+    context = {
+        'receita_id': id,
+        'receita_title': f'receita detalhada{id}',
+        'receita_description': f'esta é a descrição detalhada da receita com id {id}',
+    }

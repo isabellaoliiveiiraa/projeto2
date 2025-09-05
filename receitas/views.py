7ca96a312b8c5952e34d38594985825f2bee2a90
isabellaoliiveiiraa <bellacsoliveira123@gmail.com>
2025-09-04 21:09:29 -0300

    terminado

--- receitas/views.py
+++ receitas/views.py
@@ -2,27 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from .models import Receita
 
 def home(request):
-    receitas = Receita.objects.all().order_by('-created_at')
-    context = {
-        'receitas': receitas
-    }
-    return render(request, 'receitas/home.html', context)
+    receitas = Receita.objects.all()
+    return render(request, 'receitas/home.html', {'receitas': receitas})
 
 def receita_detail(request, id):
     receita = get_object_or_404(Receita, pk=id)
-    context = {
-        'receita': receita,
-    }
-    return render(request, 'receitas/receita_detail.html', context)
+    return render(request, 'receitas/receita_detail.html', {'receita': receita})
 
 def pesquisar_receitas(request):
-    query = request.GET.get('q')
+    query = request.GET.get('q', '')
     resultados = []
     if query:
         resultados = Receita.objects.filter(title__icontains=query)
-
-    context = {
+    return render(request, 'receitas/pesquisa.html', {
         'query': query,
-        'resultados': resultados,
-    }
-    return render(request, 'receitas/pesquisa.html', context)
+        'receitas': resultados
+    })
